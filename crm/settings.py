2801f9f9bc28f6187823c52227fdf39829744c3a
Kru <Kru@Kru-MacBook-Air.local>
2022-03-29 13:30:36 +0530

    settings changes done

--- crm/settings.py
+++ crm/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -125,6 +126,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
+
+STATIC_ROOT =os.path.join(BASE_DIR, 'static') 
 
 STATIC_URL = 'static/'
 MEDIA_URL='/images/'
